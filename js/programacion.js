6aa8c2d19a8547e46972e0062de0090141af2df8
GraceDietz <gradietz1@gmail.com>
2023-06-10 11:23:11 -0300

    archivo programacion.js sin funcionar focus del formulario

--- js/programacion.js
+++ js/programacion.js
@@ -1,8 +1,7 @@
-//hago la carga 
+// archivo javascript de la pagina comprartickets
 
 
 const formulario=document.querySelector("#form");
-console.log(formulario);
 const nombre=document.getElementById("input_nombre");
 const apellido=document.getElementById("input_apellido");
 const email=document.getElementById("input_email");
@@ -12,69 +11,81 @@ const totalAPagar=document.getElementById("totalpagar");
 const boton = document.getElementById("resumen");
 const borrar=document.getElementById("borrar");
 
+//ingreso de nombre y validacion
+nombre.addEventListener('blur',function(){
+    const nombreValue = nombre.value;
+    if((!validarString(nombreValue))) {
+        
+        alert( 'Debe ingresar un nombre valido');
+        setTimeout(function() {nombre.focus();},100);
+        
+    };
+});
 
 
+//ingreso de apellido y validacion
+ apellido.addEventListener('blur',function(){
+        const apellidoValue = apellido.value;
+        if((!validarString(apellidoValue))) {
+            
+            alert( 'Debe ingresar un apellido válido');
+            setTimeout(function() {apellido.focus();},100);
+            
+        };
  
+});
+
+
+// ingreso de email y validacion
+email.addEventListener('blur',function(){
+    const emailValue = email.value;
+    if((!isEmail(emailValue))) {
+        
+        alert( 'Debe ingresar un email válido');
+        setTimeout(function() {email.focus();},100);
+        
+    };
+
+});
+
+//ingreso de cantidad y validacion
+cantidad.addEventListener('blur',function(){
+    const cantidadcompra=parseInt(cantidad.value);
+   
+   
+    if (cantidadcompra<1) {
+		alert( 'Ingrese una cantidad válida ');
+       
+        setTimeout(function() {cantidad.focus();},100);
+        
+    };
+
+});
+	
+
 boton.addEventListener('click',Calcular) ;
 borrar.addEventListener('click', borrarCampos);
 //funcion calcula pagar
+
 function Calcular(){
-	
-    alert("¡Hiciste clic en el botón!");
-	checkInputs();
     const cantidadcomprar=parseInt(cantidad.value);
     const categoriaPago=option.value;
     let sonpesos=calcularMontoTotal(categoriaPago, cantidadcomprar);
-   
-   
     const pagar=document.getElementById("totalPagar");
     pagar.setAttribute("placeholder",(`Total a pagar : \$ ${sonpesos}`));
-    
-   
+      
 };
 
 
 
     //Funcion limpiar formulario
 function borrarCampos() {
-       
         document.getElementById('form').reset();
-
-        console.log("entra en borrar campos");
-
-      document.getElementById('totalPagar').value="Total a pagar : $ ";
-
-        
-      
+        document.getElementById('totalPagar').value="Total a pagar : $  ";
+  
     };
    
 
-//funcion para chequear los inputs y enviar mensaje de alert
-function checkInputs() {
-	
-	const nombreValue = nombre.value;
-    const apellidoValue = apellido.value;
-	const emailValue = email.value;
-	const cantidadcompra=parseInt(cantidad);
-   
-	
-	if(nombreValue === ''||(!validarString(nombreValue))) {
-		alert( 'Debe ingresar un nombre valido');
-	} ;       	
-	if(apellidoValue === ''||(!validarString(apellidoValue))) {
-		alert('Debe ingresar un apellido valido');
-    };
-        
-	if(emailValue === ''|| (!isEmail(emailValue))) {
-        alert('Debe ingresar un email válido');
-    };
-		
-	if(cantidadcompra === ''|| (cantidadcompra<0)) {
-		alert( 'Ingrese una cantidad válida ')
-        cantidadcompra=0;
-	} ;
-	
-};
 // funcion para calcular el monto a pagar segun categorias retorna monto calculado
 function calcularMontoTotal(option, cantidadcompra) {
     let suPagoEs = 0;
@@ -119,6 +130,50 @@ function isEmail(email) {
 
 
 
+/*
+
+const form = document.querySelector('form');
+const inputs = form.querySelectorAll('input');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  for(let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    const inputValue = input.value.trim();
+
+    switch(input.name) {
+      case 'nombre':
+      case 'apellido':
+        if(!validarString(inputValue)) {
+          alert(`Ingrese un ${input.name} válido`);
+          input.focus();
+          return;
+        }
+        break;
+      case 'email':
+        if(!isEmail(inputValue)) {
+          alert('Ingrese un email válido');
+          input.focus();
+          return;
+        }
+        break;
+      case 'cantidad':
+        if(parseInt(inputValue) < 1) {
+          alert('Ingrese una cantidad válida');
+          input.focus();
+          return;
+        }
+        break;
+    }
+  }
+
+  // Si llegamos aquí, todos los campos son válidos
+  alert('Formulario enviado correctamente');
+});
+
+
+*/
 
 
 
